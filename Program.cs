67c5cdf06165a7b2a4172bc8acc2d78df79f70d4
Jeremy <103245650+Jlamb96@users.noreply.github.com>
2023-04-23 23:59:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello to Jeremy's Tip Calculator.");
+Console.Write("How much is the total of the bill? ");
+int totalBill = Convert.ToInt32(Console.ReadLine());    
+Console.Write("How many people are paying the bill? ");
+int peoplePaying = Convert.ToInt32(Console.ReadLine());
+Console.Write("What percentage do you want to tip? ");
+float tipPercentage = Convert.ToSingle(Console.ReadLine());
+tipPercentage = Convert.ToSingle(tipPercentage * .01);
+float tipAmount = (totalBill / peoplePaying) * tipPercentage; 
+
+Console.WriteLine("The amount to tip for each person is " + tipAmount);
+  
